7e834c13eb0cef47d80b7e5b295e6832654ac1f0
bird <mostafa_shaeri_tj@yahoo.com>
2022-05-26 15:29:07 +0430

    Find Max Profit done

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
 // returns cut lenght, remained trunck length, remained cutter length
 func cut(trunk int, cutterLength int) (int, int, int) {
 
@@ -10,6 +17,7 @@ func cut(trunk int, cutterLength int) (int, int, int) {
 	}
 }
 
+// calculate the profit of a permutation of trunks
 func ProfitCalculator(trunks []int) int {
 	totalProfit := 0
 	currentcutterLength := 3
@@ -32,4 +40,52 @@ func ProfitCalculator(trunks []int) int {
 	}
 
 	return totalProfit
+}
+
+// permutation of trunks thrown in river
+func permutation(trunks []int) (permuts [][]int) {
+	var rc func([]int, int)
+	rc = func(a []int, k int) {
+		if k == len(a) {
+			permuts = append(permuts, append([]int{}, a...))
+		} else {
+			for i := k; i < len(trunks); i++ {
+				a[k], a[i] = a[i], a[k]
+				rc(a, k+1)
+				a[k], a[i] = a[i], a[k]
+			}
+		}
+	}
+	rc(trunks, 0)
+
+	return permuts
+}
+
+func findMaxProfit(trunks []int) int {
+	trunksPerms := permutation(trunks)
+	maxProfit := 0
+	for i := 0; i < len(trunksPerms); i++ {
+		profit := ProfitCalculator(trunksPerms[i])
+		if profit > maxProfit {
+			maxProfit = profit
+		}
+	}
+
+	return maxProfit
+}
+
+func main() {
+	type Cases struct {
+		CaseNumber int
+		sawmills   [][]int
+	}
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Give Z : ")
+	Z, _ := reader.ReadString('\n')
+	sawmillsCount, _ := strconv.Atoi(Z)
+	for i := 0; i < sawmillsCount; i++ {
+		trunks, _ := reader.ReadString('\n')
+		fmt.Printf(trunks)
+	}
+
 }
--- sawmills_test.go
+++ sawmills_test.go
@@ -7,16 +7,26 @@ type ProfitTest struct {
 	expected int
 }
 
-var ProfitTests = []ProfitTest{
-	ProfitTest{[]int{2, 3, 1}, 4},
-	ProfitTest{[]int{1, 3, 2}, 4},
-	ProfitTest{[]int{1, 4}, 8},
-}
-
 func TestCalcSeries(t *testing.T) {
-
+	var ProfitTests = []ProfitTest{
+		ProfitTest{[]int{2, 3, 1}, 4},
+		ProfitTest{[]int{1, 3, 2}, 4},
+		ProfitTest{[]int{1, 4}, 5},
+	}
 	for _, test := range ProfitTests {
 		if output := ProfitCalculator(test.trunk); output != test.expected {
+			t.Errorf("Output %d not equal to expected %d", output, test.expected)
+		}
+	}
+}
+
+func TestFindMaxProfit(t *testing.T) {
+	var ProfitTests = []ProfitTest{
+		ProfitTest{[]int{1, 2, 3}, 4},
+		ProfitTest{[]int{4, 1}, 5},
+	}
+	for _, test := range ProfitTests {
+		if output := findMaxProfit(test.trunk); output != test.expected {
 			t.Errorf("Output %d not equal to expected %d", output, test.expected)
 		}
 	}
