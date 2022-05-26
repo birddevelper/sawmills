c7d97f1a10d1c0902fb1622a2e4ecf3eb3269e82
bird <mostafa_shaeri_tj@yahoo.com>
2022-05-26 16:38:21 +0430

    comments added

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 
 // returns cut lenght, remained trunck length, remained cutter length
 func cut(trunk int, cutterLength int) (int, int, int) {
-
+	// if trunk length is less than cutrer, return whole trunk length, otherwise retuen cutterLength
 	if cutterLength >= trunk {
 		return trunk, 0, cutterLength - trunk
 	} else {
@@ -21,13 +21,17 @@ func cut(trunk int, cutterLength int) (int, int, int) {
 // calculate the profit of a permutation of trunks
 func ProfitCalculator(trunks []int) int {
 	totalProfit := 0
+	//at the first the cutter has to cut 3 block
 	currentcutterLength := 3
 	for _, trunk := range trunks {
-
+		// at the first, remained trunk length is whole the trunk
 		trunkRemainedLength := trunk
 		for trunkRemainedLength > 0 {
 			var cutLength int
+			// do cut the trunk
 			cutLength, trunkRemainedLength, currentcutterLength = cut(trunkRemainedLength, currentcutterLength)
+
+			// give bounce or penalty on cutted trunck length
 			if cutLength == 1 {
 				totalProfit += -1
 			} else if cutLength == 2 {
@@ -46,6 +50,7 @@ func ProfitCalculator(trunks []int) int {
 // permutation of trunks thrown in river
 func permutation(trunks []int) (permuts [][]int) {
 	var rc func([]int, int)
+	// calculate all permutations of the trunks sequence
 	rc = func(a []int, k int) {
 		if k == len(a) {
 			permuts = append(permuts, append([]int{}, a...))
@@ -62,9 +67,12 @@ func permutation(trunks []int) (permuts [][]int) {
 	return permuts
 }
 
+// find maximum profit between all permutation of a trunk sequence
 func findMaxProfit(trunks []int) int {
+	// calculate permutation
 	trunksPerms := permutation(trunks)
 	maxProfit := 0
+	//find maximum profit
 	for i := 0; i < len(trunksPerms); i++ {
 		profit := ProfitCalculator(trunksPerms[i])
 		if profit > maxProfit {
@@ -75,8 +83,10 @@ func findMaxProfit(trunks []int) int {
 	return maxProfit
 }
 
+// find permutation which has profit equal to maximum profit
 func findPermutionWithMaxProfit(trunks []int) [][]int {
 	var permutionWithMaxProfit [][]int
+	// find maximum profit of this sequence
 	maxProfit := findMaxProfit(trunks)
 	trunksPerms := permutation(trunks)
 	for i := 0; i < len(trunksPerms); i++ {
@@ -90,25 +100,28 @@ func findPermutionWithMaxProfit(trunks []int) [][]int {
 }
 
 func main() {
+	// define cases
 	type Case struct {
 		CaseNumber int
 		sawmills   [][]int
 	}
 
 	var Cases []Case
-
+	// read from input
 	reader_1 := bufio.NewReader(os.Stdin)
 	fmt.Print("Give Z : ")
 	Z, _ := reader_1.ReadString('\n')
 	caseNo := 0
-
+	// convert read string to int
 	sawmillsCount, _ := strconv.Atoi(strings.Replace(Z, "\r\n", "", -1))
 
+	// Take Input until Z = 0
 	for sawmillsCount != 0 {
 
 		caseNo += 1
 		var sawmills [][]int
 		for i := 0; i < sawmillsCount; i++ {
+			// for each sawmill read the trunk sequence from input
 			reader_2 := bufio.NewReader(os.Stdin)
 			trunks, _ := reader_2.ReadString('\n')
 			trunksData := strings.Fields(trunks)
@@ -122,11 +135,13 @@ func main() {
 		myCase := Case{caseNo, sawmills}
 		Cases = append(Cases, myCase)
 
+		// get Z from input for next round, you can pass 0 to finish input
 		fmt.Print("Give Z : ")
 		Z, _ := reader_1.ReadString('\n')
 		sawmillsCount, _ = strconv.Atoi(strings.Replace(Z, "\r\n", "", -1))
 	}
 
+	// calculating the requested output
 	for i, cs := range Cases {
 		perms_squence := ""
 		fmt.Println("Case", i)
